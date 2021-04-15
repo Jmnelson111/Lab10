708a4e18cc69b150b84c0e0a7484529f732d76d5
Joseph <josephnm@umich.edu>
2021-04-15 01:59:20 -0400

    Files Added

---
+++ testSelectionSort.java
@@ -0,0 +1,23 @@
+package lab10;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testSelectionSort {
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixed();
+		testDuplicates();
+	}
+
+    public testSelectionSort() {
+        
+    }
+    
+    
+    }
+
+
+}
